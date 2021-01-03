8f26cad75e9dd80bc24fd4bce7c18048b52355e4
lexx54 <chapelexx54@gmail.com>
2021-01-03 16:36:11 -0400

    added arg type to retrive function

--- scripts/display.js
+++ scripts/display.js
@@ -12,13 +12,14 @@ export function displayAll(){
   $container.children[0].innerHTML=`<input type="text" placeholder="add details" class="display-input" name="info">
   <label class="display-label" name="info">Add</label>`;
 
-  retrive();
+  retrive("all");
   console.log("all")
 }
 
 export function displayActive(){
   $container.children[0].innerHTML=`<input type="text" placeholder="add details" class="display-input" name="info">
   <label class="display-label" name="info">Add</label>`;
+  retrive("act")
   console.log("active")
 }
 export function displayCompleted(){
@@ -26,15 +27,15 @@ export function displayCompleted(){
   console.log("completed")
 }
 
-function retrive(){
+function retrive(type){
   let data=localStorage.getItem("tasks")
   ? localStorage.getItem("tasks").split(",")
   :[];
 
   let erase=$container.children[1];
   while(erase.firstChild) erase.removeChild(erase.lastChild); //erase all the elements before apply more
-  
 
+  if(type==="all"){
     for (let i=0; i<data.length; i++){
       const $para=d.createElement("p"),
         $btn=d.createElement("input"),
@@ -47,6 +48,24 @@ function retrive(){
       $para.appendChild($label);
 
       $container.children[1].appendChild($para)
+    }
+  } else if( type==='act'){
+    let data2=data.filter(ele=>ele.className!=='completed')
+    for (let i=0; i<data2.length; i++){
+      const $para=d.createElement("p"),
+        $btn=d.createElement("input"),
+        $label=d.createElement("label");
+
+      $btn.type="checkbox";
+      $btn.className="btn-in"
+      $label.textContent=`${data2[i]}`
+      $para.appendChild($btn);
+      $para.appendChild($label);
+
+      $container.children[1].appendChild($para)
     
+    }
   }
+
+    
 }
