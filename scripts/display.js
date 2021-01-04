6259912f046281fad135d62811596b3dae2b6d69
lexx54 <chapelexx54@gmail.com>
2021-01-03 20:12:21 -0400

    now all active display all the information according the selected one

--- scripts/display.js
+++ scripts/display.js
@@ -17,45 +17,80 @@ export function displayActive(){
 }
 export function displayCompleted(){
   $container.children[0].innerHTML="";
+  retrive("com");
   console.log("completed")
 }
 
 function retrive(type){
-  let data=localStorage.getItem("tasks")
+  let task=localStorage.getItem("tasks")
   ? localStorage.getItem("tasks").split(",")
+  :[];
+
+  let completed=localStorage.getItem("completed")
+  ? localStorage.getItem("completed").split(",")
   :[];
 
   let erase=$container.children[1];
   while(erase.firstChild) erase.removeChild(erase.lastChild); //erase all the elements before apply more
 
   if(type==="all"){
-    for (let i=0; i<data.length; i++){
+    for (let i=0; i<task.length; i++){
       const $para=d.createElement("p"),
         $btn=d.createElement("input"),
         $label=d.createElement("label");
 
       $btn.type="checkbox";
       $btn.className="btn-in"
-      $label.textContent=`${data[i]}`
+      $label.textContent=`${task[i]}`
+      $para.appendChild($btn);
+      $para.appendChild($label);
+
+      $container.children[1].appendChild($para)
+    }
+    for (let i=0; i<completed.length; i++){
+      const $para=d.createElement("p"),
+        $btn=d.createElement("input"),
+        $label=d.createElement("label");
+
+      $btn.type="checkbox";
+      $btn.className="btn-in";
+      $btn.checked='checked';
+      $label.className="completed"
+      $label.textContent=`${completed[i]}`
       $para.appendChild($btn);
       $para.appendChild($label);
 
       $container.children[1].appendChild($para)
     }
   } else if( type==='act'){
-    let data2=data.filter(ele=>ele.className!=='completed')
-    for (let i=0; i<data2.length; i++){
+    for (let i=0; i<task.length; i++){
       const $para=d.createElement("p"),
         $btn=d.createElement("input"),
         $label=d.createElement("label");
 
       $btn.type="checkbox";
       $btn.className="btn-in"
-      $label.textContent=`${data2[i]}`
+      $label.textContent=`${task[i]}`
       $para.appendChild($btn);
       $para.appendChild($label);
 
       $container.children[1].appendChild($para)
     }
-  }    
+  } else if(type==="com"){
+    for (let i=0; i<completed.length; i++){
+      const $para=d.createElement("p"),
+        $btn=d.createElement("input"),
+        $label=d.createElement("label");
+
+      $btn.type="checkbox";
+      $btn.className="btn-in";
+      $btn.checked='checked';
+      $label.className="completed"
+      $label.textContent=`${completed[i]}`
+      $para.appendChild($btn);
+      $para.appendChild($label);
+
+      $container.children[1].appendChild($para)
+    }
+  }
 }
